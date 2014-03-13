1ee75494bf5300ea2b9135f0985061f61370fe3d
John Arne Øye <johnarneoye@gmail.com>
2014-03-13 13:58:58 +0100

    added oauth

--- twitter/retriever_tweepy.py
+++ twitter/retriever_tweepy.py
@@ -5,3 +5,11 @@ Created on 10. mars 2014
 '''
 import tweepy
 
+OAUTH_API_KEY = "JvgeRvICbMtWYcmhTug3w"
+OAUTH_API_SECRET = "CzIwJm5yUi6hTHeLjrYMHZIMoszkNCD1MqgHFfO5qI"
+
+class TweetRetriever():
+    
+    def __init__(self):
+        auth = tweepy.OAuthHandler(OAUTH_API_KEY, OAUTH_API_SECRET)
+        
