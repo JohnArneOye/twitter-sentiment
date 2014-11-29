cab21c09a56e6ce6fe3342259fc0f6bed32016be
John Arne Øye <johnarneoye@gmail.com>
2014-11-29 16:03:05 +0100

    lexicon and feature extraction

---
+++ feature_extraction.py
@@ -0,0 +1,50 @@
+'''
+Created on 27. nov. 2014
+
+@author: JohnArne
+'''
+
+def get_feature_set_A(tweets):
+    """
+    Retrieves a list of tweets objects and returns feature set A.
+    """
+    features= []
+    
+    
+    return features
+
+def get_feature_set_B(tweets):
+    """
+    Retrieves a list of tweets objects and returns feature set B.
+    """
+    features= []
+    
+    
+    return features
+
+def get_feature_set_C(tweets):
+    """
+    Retrieves a list of tweets objects and returns feature set C.
+    """
+    features= []
+    
+    
+    return features
+
+def get_feature_set_D(tweets):
+    """
+    Retrieves a list of tweets objects and returns feature set D.
+    """
+    features= []
+    
+    
+    return features
+
+def get_feature_set_E(tweets):
+    """
+    Retrieves a list of tweets objects and returns feature set E.
+    """
+    features= []
+    
+    
+    return features
