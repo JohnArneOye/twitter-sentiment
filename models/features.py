adc8539a414a48d8618545430c320801fd9a0255
John Arne Øye <johnarneoye@gmail.com>
2014-12-02 21:26:40 +0100

    more nb

---
+++ models/features.py
@@ -0,0 +1,43 @@
+'''
+Created on 27. nov. 2014
+
+@author: JohnArne
+'''
+
+def get_feature_set(tweets,featureset):
+    if(featureset=='A'):
+        return get_feature_set_A(tweets)
+    elif(featureset=='B'):
+        return get_feature_set_B(tweets)
+    elif(featureset=='C'):
+        return get_feature_set_C(tweets)
+
+def get_feature_set_A(tweets):
+    """
+    Retrieves a list of tweets objects and returns feature set A.
+    """
+    print "Getting feature set A"
+    features= []
+    
+    
+    return features
+
+def get_feature_set_B(tweets):
+    """
+    Retrieves a list of tweets objects and returns feature set B.
+    """
+    print "Getting feature set A"
+    features= []
+    
+    
+    return features
+
+def get_feature_set_C(tweets):
+    """
+    Retrieves a list of tweets objects and returns feature set C.
+    """
+    print "Getting feature set A"
+    features= []
+    
+    
+    return features
